1d5d7dc4ea49cc96da33247c992fe9e28dc36008
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-10-19 10:43:07 -0300

    Modificado layout

--- pedroalpacheco/src/componentes/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import logo from '../logo.svg';
-import TweenMax from "gsap/TweenMax";
-
-export default function(){
-    return <img src={logo} className="App-logo" alt="logo" />
-}
